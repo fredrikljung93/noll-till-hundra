3bf557dc6c47336ec0eaa88c9d1a495d38fb2c8d
Fredrik Ljung <fredrik.ljung@tellox.se>
2024-08-16 21:54:10 +0200

    Store theme choice in local storage (#19)

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import { Elm } from './src/Main.elm'
 
-Elm.Main.init({
-  node: document.querySelector('main')
-})
+const storedTheme = localStorage.getItem('THEME');
+
+var app = Elm.Main.init({
+  node: document.querySelector('main'), 
+  flags: storedTheme
+});
+
+
+app.ports.saveProperty.subscribe(function(tuple) {
+    const key = tuple[0];
+    const value = tuple[1];
+    localStorage.setItem(key,value);
+});
+
